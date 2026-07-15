82f09baaa9a3072aa9ec38e39c862deecc01ee66
João Sanson <joaosanson01@gmail.com>
2026-07-14 23:07:13 -0300

    feat(database): setup postgres connection handling

---
+++ internal/database/postgres.go
@@ -0,0 +1,25 @@
+// Package database used to handle database Postgres connection
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+func Connect(databaseURL string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", databaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("open database: %w", err)
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("ping database: %w", err)
+	}
+
+	return db, nil
+}
