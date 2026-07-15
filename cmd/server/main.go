4a07ad542420754d82c8cd80c648ad3ef0ba05c1
João Sanson <joaosanson01@gmail.com>
2026-07-14 23:07:13 -0300

    feat(server): serve frontend and refactor websocket route

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/joaosanson/go-websocket-study/internal/chat"
+)
+
 func main() {
+	hub := chat.NewHub()
+	chatHandler := chat.NewHandler(hub)
+
+	go hub.Run()
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /ws/{roomID}", chatHandler.ServeWS)
+	
+	// Serve the React frontend build
+	fs := http.FileServer(http.Dir("./frontend/dist"))
+	mux.Handle("/", fs)
+
+	log.Println("server is running on port :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
 }
